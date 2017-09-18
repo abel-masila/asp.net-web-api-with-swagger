d6d7b5b416a1ed2e108e124206d6c64b21f754d0
abel-masila <abelmasila@gmail.com>
2017-09-18 19:50:22 +0300

    Final Api

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+        public TodoController(TodoContext todoContext)
+        {
+            _context = todoContext;
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item 1" });
+                _context.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }/// <summary>
+         /// Creates a TodoItem
+         /// </summary>
+         /// <remarks>
+         /// Sample Request
+         /// 
+         ///     Post /Todo
+         ///     {
+         ///         "id":1,
+         ///         "Name":"Item 1",
+         ///         "isCompleted":true
+         ///     }
+         /// </remarks>
+         /// <param name="item"></param>
+         /// <returns>A newly-created TodoItem</returns>
+         /// <response code="201">Returns the newly-created item</response>
+         /// <response code="400">If the item is null</response>  
+        [HttpPost]
+        [ProducesResponseType(typeof(TodoItem),201)]
+        [ProducesResponseType(typeof(TodoItem), 400)]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            todo.isComplete = item.isComplete;
+            todo.Name = item.Name;
+
+            _context.TodoItems.Update(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+        /// <summary>
+        /// Deletes a specific TodoItem
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+
+        }
+    }
+}
---
+++ TodoApi/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using System.IO;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt=>opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc();
+            //Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c=> {
+                c.SwaggerDoc("v1", new Info {
+                    Title = "TodoApi",
+                    Version = "v1",
+                    Description="A simple .net core v2.0 Todo web api with CRUD",
+                    TermsOfService="None",
+                    Contact=new Contact { Name="Abel Masila",Email="abelmasila@gmail.com", Url= "https://abel-masila.github.io/" },
+                    License = new License { Name = "Use under GPLv3 ", Url = "https://choosealicense.com/licenses/gpl-3.0/" }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "TodoApi.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c=> {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json","TodoApi V1");
+            });
+            app.UseMvc();
+        }
+    }
+}
